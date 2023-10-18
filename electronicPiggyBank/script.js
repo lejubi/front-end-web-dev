24909d2a34f4c9f2cdcadf58a70085e6ff651d8e
lejubi <junalee515@gmail.com>
2023-10-17 23:05:57 -0400

    ghkjlk

--- electronicPiggyBank/script.js
+++ electronicPiggyBank/script.js
@@ -20,3 +20,28 @@ Some other helpful functions:
 str.toLowerCase() //turns the string str into lowercase 
 .toFixed(2) // rounds a number to 2 decimal places
  */
+
+let piggyBankAmount = 0.0;
+let button = document.getElementById("coinEnter");
+let input = document.getElementById("coinInput");
+
+button.addEventListener("click", function(){})
+
+
+
+// coinInput.addEventListener("click", readInput)
+
+// function readInput(){
+//     let inputText = document.getElementById("coinInput");
+//     let input = inputText.value.toLowerCase();
+//     addInput(input);
+// }
+
+// function addInput(input){
+//     let balance = document.getElementById("piggyBank");
+//     if (input == "p") balance += 0.01;
+//     else if (input == "n") balance += 0.05;
+//     else if (input == "d") balance += 0.1;
+//     else if (input == "q") balance += 0.25;
+//     console.log(balance)
+// }
